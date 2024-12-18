a8f4ec6e5532a6854edd41e665150b15cffd3b81
bachwer <110678064+bachwer@users.noreply.github.com>
2024-12-18 15:09:24 +0700

    Update [Cơ bản] 1. Làm quen với cấu trúc.c

--- [Cơ bản] 1. Làm quen với cấu trúc.c
+++ [Cơ bản] 1. Làm quen với cấu trúc.c
@@ -6,14 +6,12 @@ int main(void) {
         int age;
         char phoneNumber[10];
     };
-    struct information person1;
-    printf("Enter your name: ");
-    fgets(person1.name, sizeof(person1.name), stdin);
-    printf("Enter your age: ");
-    scanf("%d", &person1.age);
-    fflush(stdin);
-    printf("Enter your phone number: ");
-    fgets(person1.phoneNumber, sizeof(person1.phoneNumber), stdin);
+    struct information person1 = {
+        "Nguyen van A",
+        18,
+        "090123456"
+    };
+    
     printf("Name: %s\n", person1.name);
     printf("Age: %d\n", person1.age);
     printf("Phone number: %s\n", person1.phoneNumber);
